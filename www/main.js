61e9b3c1907326439bddf7ea00300c25126c7c75
Kartikay-Srivastava <2k23.mca2312450@gmail.com>
2025-06-09 16:40:02 +0530

    Index Design

---
+++ www/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: 'bounceIn',  // correct case
+        },
+        out: {
+            effect: 'bounceOut'  // correct case
+        }
+    });
+});
+
